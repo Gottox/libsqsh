e6db0f0dc93b97afc37aaeebbb139df3722586ff
Gottox <Gottox@users.noreply.github.com>
2022-10-17 17:14:52 +0000

    deploy: 5df1c54553aa3055c6d0cd32a8f65d771b92c2a1

--- search/variables_2.js
+++ search/variables_2.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['data_0',['data',['../structSqshCanaryMapper.html#a4b9151a1a180df23b9bccc9a3907bdae',1,'SqshCanaryMapper::data()'],['../structSqshCanaryMap.html#a8f7e2392ca981b7b8eb11ac66cefce18',1,'SqshCanaryMap::data()'],['../structSqshMapping.html#a6538b9a3c1bf868043a84b7a92e4a46c',1,'SqshMapping::data()'],['../structSqshMapper.html#ab711d786d84588354497cef589cd570a',1,'SqshMapper::data()'],['../structSqshMmapFullMapper.html#a92a2a4505eb1c57aeef7ca8dc785e77e',1,'SqshMmapFullMapper::data()'],['../structSqshMmapFullMap.html#a5c131623f89743874d322a8720b658b1',1,'SqshMmapFullMap::data()'],['../structSqshMmapMap.html#ab4cc0ded33c02f2ca032eaa1ba4acc8e',1,'SqshMmapMap::data()'],['../structSqshStaticMapper.html#a1f193891a1df25207d1fa0505214bfca',1,'SqshStaticMapper::data()'],['../structSqshStaticMap.html#a7eaec24f0ff56b4cef48fcffeb99e666',1,'SqshStaticMap::data()'],['../structSqshBuffer.html#a26e2982e48dc069e2d70ff9e32703823',1,'SqshBuffer::data()']]],
   ['data_5fcompression_1',['data_compression',['../structSqsh.html#adac498959c2e8a683e0c69424466dfe6',1,'Sqsh']]],
-  ['directory_2',['directory',['../structSqshDirectoryIterator.html#a67d41d0f45a951939d2280058bc25ef5',1,'SqshDirectoryIterator']]],
-  ['dtor_3',['dtor',['../structSqshRefCount.html#a96ce91101c8e374a0270a9161b69f967',1,'SqshRefCount']]]
+  ['directory_2',['directory',['../structSqshDirectoryIterator.html#a67d41d0f45a951939d2280058bc25ef5',1,'SqshDirectoryIterator']]]
 ];
