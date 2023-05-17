dda0383fb66a1e5fc590dd246e9caebc66bc72e7
Gottox <Gottox@users.noreply.github.com>
2023-05-17 19:53:20 +0000

    deploy: cd99f0f97c0795b59396d6fb753dcb731969868b

--- search/enums_0.js
+++ search/enums_0.js
@@ -1,13 +1,12 @@
 var searchData=
 [
-  ['sqshdatainodetype_0',['SqshDataInodeType',['../sqsh__data_8h.html#a42e0085c546d98905863aef18e67cd58',1,'sqsh_data.h']]],
-  ['sqsherror_1',['SqshError',['../sqsh__error_8h.html#ac92b5f178c51aef07c08a63847b6d3cc',1,'sqsh_error.h']]],
-  ['sqshgzipstrategies_2',['SqshGzipStrategies',['../sqsh__archive_8h.html#a485fd0fb6de33ee127ffda8f1468d12a',1,'sqsh_archive.h']]],
-  ['sqshinodetype_3',['SqshInodeType',['../sqsh__inode_8h.html#a1ef4e3855eaa4bf23ed5f3a1b5b09c57',1,'sqsh_inode.h']]],
-  ['sqshlz4flags_4',['SqshLz4Flags',['../sqsh__archive_8h.html#ace71a8023ba184910ef399659ae581ae',1,'sqsh_archive.h']]],
-  ['sqshlzoalgorithm_5',['SqshLzoAlgorithm',['../sqsh__archive_8h.html#a088ced7149f379b9520210b602867c03',1,'sqsh_archive.h']]],
-  ['sqshsuperblockcompressionid_6',['SqshSuperblockCompressionId',['../sqsh__archive_8h.html#ae0979c00d3e768637a9a10f31b95594a',1,'sqsh_archive.h']]],
-  ['sqshsuperblockflags_7',['SqshSuperblockFlags',['../sqsh__archive_8h.html#af646f88052c2cec402c25f13e9497c91',1,'sqsh_archive.h']]],
-  ['sqshxattrtype_8',['SqshXattrType',['../sqsh__xattr_8h.html#af6aa1115a96a68fa5f4417984e12b923',1,'sqsh_xattr.h']]],
-  ['sqshxzfilters_9',['SqshXzFilters',['../sqsh__archive_8h.html#aa2a3ee9586d5ff541df2a603c68a24e6',1,'sqsh_archive.h']]]
+  ['sqsherror_0',['SqshError',['../sqsh__error_8h.html#ac92b5f178c51aef07c08a63847b6d3cc',1,'sqsh_error.h']]],
+  ['sqshgzipstrategies_1',['SqshGzipStrategies',['../sqsh__archive_8h.html#a485fd0fb6de33ee127ffda8f1468d12a',1,'sqsh_archive.h']]],
+  ['sqshinodetype_2',['SqshInodeType',['../sqsh__inode_8h.html#a1ef4e3855eaa4bf23ed5f3a1b5b09c57',1,'sqsh_inode.h']]],
+  ['sqshlz4flags_3',['SqshLz4Flags',['../sqsh__archive_8h.html#ace71a8023ba184910ef399659ae581ae',1,'sqsh_archive.h']]],
+  ['sqshlzoalgorithm_4',['SqshLzoAlgorithm',['../sqsh__archive_8h.html#a088ced7149f379b9520210b602867c03',1,'sqsh_archive.h']]],
+  ['sqshsuperblockcompressionid_5',['SqshSuperblockCompressionId',['../sqsh__archive_8h.html#ae0979c00d3e768637a9a10f31b95594a',1,'sqsh_archive.h']]],
+  ['sqshsuperblockflags_6',['SqshSuperblockFlags',['../sqsh__archive_8h.html#af646f88052c2cec402c25f13e9497c91',1,'sqsh_archive.h']]],
+  ['sqshxattrtype_7',['SqshXattrType',['../sqsh__xattr_8h.html#af6aa1115a96a68fa5f4417984e12b923',1,'sqsh_xattr.h']]],
+  ['sqshxzfilters_8',['SqshXzFilters',['../sqsh__archive_8h.html#aa2a3ee9586d5ff541df2a603c68a24e6',1,'sqsh_archive.h']]]
 ];
